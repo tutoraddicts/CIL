b8ce174cabdce60ce6f9e676624c41f17440e9fa
tutoraddicts <rohitdutta907@gmail.com>
2019-11-10 05:42:49 +0530

    printf function added

--- ec.c
+++ ec.c
@@ -1,18 +1,64 @@
 #include<stdio.h>
+#include<string.h>
+void known_functions(char[], char[], int);
 
 int main () {
 
 	FILE *pToFIle = fopen("net", "r");
+	int count, functions_count = 0;
 
-	int line = 0;
-
-	char input[512];
-
-	while ( fgets( input, 512, pToFIle)) {
-		line ++;
-		printf("Line:%d -> %s", line, input);
+	char input[512], functions[512];
+	
+	//while (fgets(input, 512, pToFIle)) {printf("%ld\n", strlen(input));}
+	
+	while ( fgets( input, 512, pToFIle) ) {
+		for (count = 0; count < strlen(input); count++) {
+			if ( input[count] != '\0') {
+				if (input[count] >= 97 && input[count] <= 122) {
+					//printf("working");
+						functions[functions_count] = input[count];
+						functions_count++;
+				}
+				else if (input[count] == ' ') {
+					//printf("working");
+						known_functions(functions, input, count);
+				}
+			}
+		}
 	}
 
 	fclose(pToFIle);
 	return 0;
+}
+
+void known_functions(char functions[], char input[], int count) {
+	int new_count, temp_count = 0, empty = 0;
+	//printf("working");
+	//printf("%s\n", functions);
+	//printf("%c", input[count]);
+	if (strcmp(functions, "print") == 0 && input[count + 1] == '"') {
+		new_count = count + 1;
+		//printf("working");
+		
+		if (input[new_count] == '"' && input[new_count + 1] == '"') {
+			printf(" ");
+		}
+		else {
+			for (new_count = new_count + 1; new_count < strlen(input); new_count++ ) {
+				
+				if ( input[new_count] == '"') {
+					break;
+				}
+				else {
+					functions[temp_count] = input[new_count];
+					temp_count++;
+				}
+			}
+		}
+		
+			
+
+		printf("%s", functions);
+
+	}
 }
